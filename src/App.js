defd711e83fc7759ac224de66bf04a3574b8be40
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-04-02 22:35:54 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code>
-            and save to reload.
+            and save to reload. Try again.
           </p>
           <a
             className="App-link"
