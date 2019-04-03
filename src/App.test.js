c94cf8f51f81a7cf2e4c497cb0d0a433fa54a7c9
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-03 10:46:29 +0100

    adding default testing config

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<App /> component", () => {
+  it("should render succesfuly", () => {
+    const wrapper = shallow(<App />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
 });
