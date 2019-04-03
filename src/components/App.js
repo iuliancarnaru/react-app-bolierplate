1274077ac51703ee4ecf0592200f2a85a5ada66b
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-03 10:57:21 +0100

    creating project structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../style/App.css";
+
+class App extends Component {
+  render() {
+    console.log("messy");
+
+    return (
+      <div className="App">
+        <h1>React Boilerplate</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 import Adapter from "enzyme-adapter-react-16";
-import App from "./App";
+import App from "../components/App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
