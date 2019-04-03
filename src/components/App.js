731d8e87cc0f3ea38209add1a9714f4fa8dd842e
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-03 13:08:30 +0100

    adding scss support and default style

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import "../style/App.css";
+import "../style/App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <h1>React Boilerplate</h1>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style/index.css";
+import "./style/index.scss";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
