faa7f5b0b7e18015046690ceccbb27b137f1fb05
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-16 14:32:44 +0100

    adding original structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  const userData = [{ id: 1, name: "Iulian", location: "London" }];
+
+  return (
+    <div>
+      <h1>CRUD with HOOKS</h1>
+    </div>
+  );
+};
+
+export default App;
